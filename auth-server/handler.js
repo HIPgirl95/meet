d0bea594a36d68a5477cd52220fa44bfbc945bc1
HIPgirl95 <hannah@dawne.com>
2024-11-23 12:29:36 -0500

    deploying getAuthURL

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -26,6 +26,8 @@ module.exports.getAuthURL = async () => {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Credentials": true,
     },
-    body: JSON.stringify({ authUrl }),
+    body: JSON.stringify({
+      authUrl,
+    }),
   };
 };
