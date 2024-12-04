b3c92b890f3e0a2fd266e93ec2b7f740cf967b32
HIPgirl95 <hannah@dawne.com>
2024-12-04 17:27:00 -0500

    Passing Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import EventList from "./components/EventList";
 
 const App = () => {
   return (
     <div className="App">
-      <div id="event-list"></div>
+      <EventList />
     </div>
   );
 };
