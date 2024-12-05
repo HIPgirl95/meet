3472a95a359ee77bae3dea2fbc12f2ac6ba0a3ff
HIPgirl95 <hannah@dawne.com>
2024-12-05 17:49:15 -0500

    First go at CitySearch Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
