261d9530ae11216740a1e92fe810f34013e222fd
HIPgirl95 <hannah@dawne.com>
2024-12-13 16:25:45 -0500

    Final test of feature

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -25,4 +25,10 @@ describe("show/hide an event details", () => {
     const eventDetails = await page.$(".event .details");
     expect(eventDetails).toBeDefined();
   });
+
+  test("User can collapse an event to hide details", async () => {
+    await page.click(".event .details-button");
+    const eventDetails = await page.$(".event .details");
+    expect(eventDetails).toBeNull();
+  });
 });
