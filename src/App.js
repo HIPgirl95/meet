a1dcf8f86e033d011d965ef7c996bab9846bc0cc
HIPgirl95 <hannah@dawne.com>
2024-12-30 13:12:37 -0500

    setup CityEventsChart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 
 import "./App.css";
+import CityEventsChart from "./components/CityEventsChart";
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -59,6 +60,7 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
+      <CityEventsChart allLocations={allLocations} events={events} />
       <EventList events={events} />
     </div>
   );
